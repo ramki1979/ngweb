3c9e634733df8052b56d2d1a151b50ffc9d3a6b9
ramki1979 <ramakrishna.malli@gmail.com>
2014-08-10 00:16:43 +0530

    initial commit

---
+++ src/app/app.js
@@ -0,0 +1,17 @@
+angular.module( 'ngweb', ['ui.router'])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | ngWeb' ;
+    }
+  });
+});
+
